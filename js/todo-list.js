ea28616cba7dbcadd448177e731d262d0af8f452
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-01-24 13:37:09 +0900

    practice 9

--- js/todo-list.js
+++ js/todo-list.js
@@ -1,4 +1,4 @@
-const toDoForm = document.querySelector("#todo-form");
+const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
